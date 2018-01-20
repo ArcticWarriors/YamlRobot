c1643c5b25105616fe25c7602ba1b1b1b22f282c
Andrew Johnson <iglioflop1@gmail.com>
2018-01-20 09:33:58 -0500

    First bit of yaml bot

--- src/org/snobot/YamlRobot.java
+++ src/org/snobot/YamlRobot.java
@@ -7,11 +7,17 @@ import java.util.Map;
 import org.yaml.snakeyaml.Yaml;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 
 public class YamlRobot extends IterativeRobot
 {
     private static final String sCONFIG_FILE;
+
+    private MatchMotorJoystick mGuy1;
+    private MatchMotorJoystick mGuy2;
 
     static
     {
@@ -32,16 +38,21 @@ public class YamlRobot extends IterativeRobot
      */
     public void robotInit()
     {
-        try
-        {
-            Yaml yaml = new Yaml();
-            Map<String, Object> config = (Map<String, Object>) yaml.load(new FileReader(new File(sCONFIG_FILE)));
-            System.out.println(config);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+//        try
+//        {
+//            Yaml yaml = new Yaml();
+//            Map<String, Object> config = (Map<String, Object>) yaml.load(new FileReader(new File(sCONFIG_FILE)));
+//            System.out.println(config);
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+    	Joystick joystick = new Joystick(0);
+    	SpeedController sc1 = new Talon(0);
+    	SpeedController sc2 = new Talon(1);
+    	mGuy1 = new MatchMotorJoystick(joystick, sc1);
+    	mGuy2 = new MatchMotorJoystick(joystick, sc2);
     }
 
     public void autonomousInit()
@@ -59,7 +70,8 @@ public class YamlRobot extends IterativeRobot
      */
     public void teleopPeriodic()
     {
-
+    	mGuy1.Update();
+    	mGuy2.Update();
     }
 }
 
